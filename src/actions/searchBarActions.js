1ec2de2adb9a928d4f454f0b8f301b2c8d0a4ff4
Benjamin Arias <benjamin.j.arias@gmail.com>
2016-10-12 12:19:26 +0200

    feat: Added the guardian API url + key

--- src/actions/searchBarActions.js
+++ src/actions/searchBarActions.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import * as types from '../constants/actionTypes';
 
 export function launchSearch(searchTerm) {
+  const guardianUrl = 'https://content.guardianapis.com/search?q=${searchTerm}&api-key=c1f53e50-8061-42ea-8938-bc68439d90cd'
   const url = `https://webhose.io/search?token=f694c46d-10ff-4385-8273-3a0bd8537f4a&format=json&q=(${searchTerm})%20site_category%3Amedia%20(site_type%3Anews%20OR%20site_type%3Ablogs)`;
   const request = axios.get(url);
   console.log('called');
