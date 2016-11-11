ab154424c8f37c948366256e1d5157350c743f4a
Benjamin Arias <benjamin.j.arias@gmail.com>
2016-11-11 12:56:13 +0100

    test: Wrote tests for Header component

--- src/components/Header.spec.js
+++ src/components/Header.spec.js
@@ -1,23 +1,20 @@
-import expect from 'expect';
+import { expect } from 'chai';
 import { shallow } from 'enzyme';
 import React from 'react';
-import 'jsdom-global/register';
 
 import Header from './Header';
 
 describe('<Header />', () => {
-  before(function() {
-    this.jsdom = require('jsdom-global')();
-  });
-  const wrapper = shallow(<Header />);
-  it('should render an h2 tag', () => {
-    expect(wrapper.find('h2').length).toEqual(1);
+  it('should render a header called \'MyNews\'', () => {
+    const wrapper = shallow(<Header />);
+    const actual = wrapper.find('h2').text();
+    const expected = 'MyNews';
+
+    expect(actual).to.equal(expected);
   });
 
-  it('should render a paragraph', () => {
-    expect(wrapper.find('p').length).toEqual(1);
-  });
-  after(function() {
-    this.jsdom();
+  it('should render one paragraph', () => {
+    const wrapper = shallow(<Header />);
+    expect(wrapper.find('p').length).to.equal(1);
   });
 });
